81116188bd0a6a15ee2926fc9fc22da8a96e28f7
DESKTOP-RFIKSFN\angel <angelopolotto@gmail.com>
2017-06-19 08:59:16 -0300

    criacao do projeto

---
+++ ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVMExample.ViewModels
+{
+    public class LoginPageViewModel : DotvvmViewModelBase
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public async Task SignInCommand()
+        {
+            try
+            {
+                Context.RedirectToUrl("www.google.com.br");
+                // Context.InterruptRequest();
+            }
+            catch (System.Exception e)
+            {
+                //throw;
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
---
+++ ViewModels/SiteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVMExample.ViewModels
+{
+    public class SiteViewModel : DotvvmViewModelBase
+    {
+    
+    }
+}
+
